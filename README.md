0b506a8aa516df53f19b2a8feaf76e1a491449c8
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-05-14 22:59:50 -0400

    updated readme file and changes some variable names to make them more descriptive

--- README.md
+++ README.md
@@ -3,11 +3,7 @@
 ## Description
 This is a note tracker. This app allows users to type in a task title and description, save it for later, or select an older note to view every detail. This works by utilizing express.js as a server, working with various end points to send the correct files based on the users selection on the front end. These routes utilize either GET, POST, or DELETE to achieve the desired functionality.
 
-## Installation
-import express, uuidv4 into your project
 
-## Usage
-In order to use this application, use git clone in your terminal to clone this repository onto your computer, after navigating to the project directory, make sure to install express.js, uuidv4.
 
 ## Images
 ![Note Tracker App](/images/note-tracker.png)
--- server.js
+++ server.js
@@ -7,20 +7,24 @@ const path = require('path')
 const tasksDb = require('./db/db.json')
 const { v4: uuidv4 } = require('uuid');
 
-//allowing the application to have access to public folder/converting responses to json format
+//Allowing app to use files in public folder
 app.use(express.static('public'))
+//Converting responses to JSON
 app.use(express.json())
 
 
-// api paths
 
+//API Paths
+
+//Read db.json file and send data as a response
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (error, data) => {
-        const taskData = JSON.parse(data)
-        res.json(taskData)
+        const noteData = JSON.parse(data)
+        res.json(noteData)
     })
 })
 
+//filters out notes, returns notes that the id matches the req.params.id and sends it as a response
 app.get('/api/notes/:id', (req, res) => {
     const targetedId = req.params.id
     fs.readFile('./db/db.json', (error, data) => {
@@ -28,18 +32,19 @@ app.get('/api/notes/:id', (req, res) => {
             console.error(error)
         } else {
             const parsedData = JSON.parse(data)
-            const filteredTaskId = parsedData.find((task) => task.id === targetedId)
+            const filteredNoteId = parsedData.find((note) => note.id === targetedId)
 
-            res.json(filteredTaskId)
+            res.json(filteredNoteId)
         }
     })
 })
 
+//takes in the data from the req, reads the current db.json file, writes a new file adding the req data into it
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body
 
     if (title && text) {
-        const newTask = {
+        const newNote = {
             title,
             text,
             id: uuidv4()
@@ -48,50 +53,57 @@ app.post('/api/notes', (req, res) => {
             if (err) {
                 console.error(err)
             } else {
-                const parsedTask = JSON.parse(data)
-                parsedTask.push(newTask)
+                const parsedNote = JSON.parse(data)
+                parsedNote.push(newNote)
 
-                fs.writeFile('./db/db.json', JSON.stringify(parsedTask, null, 4), (error) => error ? console.error(error) : console.info('Task added to file'))
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNote, null, 4), (error) => error ? console.error(error) : console.info('Note added to file'))
             }
         })
-
+        //sends the response to the front end
         const response = {
             status: 'success',
-            body: newTask
+            body: newNote
         }
         console.log(response)
         res.status(201).json(response)
         //stay here
     } else {
-        res.status(500).json('Error in adding a new task')
+        res.status(500).json('Error in adding a new note')
     }
 })
 
+//filters out the targeted id and sends the updated file as a response
 app.delete('/api/notes/:id', (req, res) => {
-    const taskId = req.params.id
+    const reqNoteId = req.params.id
 
     fs.readFile('./db/db.json', 'utf8', (error, data) => {
-        const taskData = JSON.parse(data)
-        const filteredTaskData = taskData.filter((task) => task.id !== taskId)
+        const noteData = JSON.parse(data)
+        const filteredNoteData = noteData.filter((task) => task.id !== reqNoteId)
 
-        fs.writeFile('./db/db.json', JSON.stringify(filteredTaskData, null, 4), (error) => {
+        fs.writeFile('./db/db.json', JSON.stringify(filteredNoteData, null, 4), (error) => {
             if (error) {
                 console.error(error)
             } else {
-                console.info('task deleted')
-                res.json({ message: 'Task deleted' })
+                console.info('Note deleted')
+                res.json({ message: 'Note deleted' })
             }
         })
     })
 })
 
+//HTML ROUTES
+
+//Sends notes.html to front end
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
+//sends index.html to front end
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+
+//listening for PORT
 app.listen(PORT, () => {
     console.log(`listening for http://localhost:${PORT}`)
 })
